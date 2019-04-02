2e4e9a5dd607dad93355e5a32b9945bfca15f5ec
Seung-Ik Ha <gktmddlr@gmail.com>
2019-04-03 01:11:25 +0900

    first commit about version1

--- BullsEye2/ViewController.swift
+++ BullsEye2/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+  
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Hello World!",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome",
+                                   style: .default,
+                                   handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
